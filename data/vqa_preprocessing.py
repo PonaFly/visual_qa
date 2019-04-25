0dce83ad254e1d55d549acebb2146dc74e4c3326
PonaFly <lolchocolatelol@gmail.com>
2019-04-25 12:26:48 +0000

    new prepr.

--- data/vqa_preprocessing.py
+++ data/vqa_preprocessing.py
@@ -17,7 +17,7 @@ def download_vqa():
 
     
 def main(params):
-    if params['download'] == 'True':
+    if params.download:
         download_vqa()
 
     '''
@@ -28,18 +28,19 @@ def main(params):
     test = []
     imdir='%s/COCO_%s_%012d.jpg'
 
-    if params['split'] == 1:
+    if params.split == 2:
 
         print('Loading annotations and questions...')
         train_anno = json.load(open('annotations/v2_mscoco_train2014_annotations.json', 'r'))
         val_anno = json.load(open('annotations/v2_mscoco_val2014_annotations.json', 'r'))
-
+        
+        test_ques = json.load(open('annotations/v2_OpenEnded_mscoco_test2015_questions.json', 'r'))
         train_ques = json.load(open('annotations/v2_OpenEnded_mscoco_train2014_questions.json', 'r'))
         val_ques = json.load(open('annotations/v2_OpenEnded_mscoco_val2014_questions.json', 'r'))
-
+        
         subtype = 'train2014'
         for i in range(len(train_anno['annotations'])):
-            if (params['binary'] == 'True' and train_anno['annotations'][i]['answer_type'] != 'yes/no'):
+            if (params.binary and train_anno['annotations'][i]['answer_type'] != 'yes/no'):
                 continue
             ans = train_anno['annotations'][i]['multiple_choice_answer']
             question_id = train_anno['annotations'][i]['question_id']
@@ -47,25 +48,26 @@ def main(params):
 
             question = train_ques['questions'][i]['question']
             print(train_ques['questions'][i])
-            #mc_ans = train_ques['questions'][i]['multiple_choices']
-
-            #train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans, 'ans': ans})
             train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
         
         subtype = 'val2014'
         for i in range(len(val_anno['annotations'])):
-            if (params['binary'] == 'True' and train_anno['annotations'][i]['answer_type'] != 'yes/no'):
+            if (params.binary and val_anno['annotations'][i]['answer_type'] != 'yes/no'):
                 continue
             ans = val_anno['annotations'][i]['multiple_choice_answer']
             question_id = val_anno['annotations'][i]['question_id']
             image_path = imdir%(subtype, subtype, val_anno['annotations'][i]['image_id'])
 
             question = val_ques['questions'][i]['question']
-            #mc_ans = val_ques['questions'][i]['multiple_choices']
+            train.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
+       
+        subtype = 'test2015'
+        for i in range(len(test_ques['questions'])):
+            question_id = test_ques['questions'][i]['question_id']
+            image_path = imdir%(subtype, subtype, test_ques['questions'][i]['image_id'])
 
-            #test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'MC_ans': mc_ans})
-            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question, 'ans': ans})
-   
+            question = test_ques['questions'][i]['question']
+            test.append({'ques_id': question_id, 'img_path': image_path, 'question': question})
 
     print('Training sample %d, Testing sample %d...' %(len(train), len(test)))
 
@@ -77,16 +79,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # input json
-    parser.add_argument('--download', default='False', help='Download and extract data from VQA server')
-    parser.add_argument('--split', default=1, type=int, help='1: train on Train and test on Val, 2: train on Train+Val and test on Test')
-    parser.add_argument('--binary', default='False', help='for binary classification')
+    parser.add_argument('--download', action='store_true', default=False, help='Download and extract data from VQA server')
+    parser.add_argument('--split', default=2, type=int, help='1: train on Train and test on Val, 2: train on Train+Val and test on Test')
+    parser.add_argument('--binary',action='store_true', default=False, help='for binary classification')
   
     args = parser.parse_args()
-    params = vars(args)
+    
     print('parsed input parameters:')
-    print( json.dumps(params, indent = 2))
-    main(params)
-
+    print(json.dumps(vars(args), indent = 2))
+    main(args)
 
 
 
